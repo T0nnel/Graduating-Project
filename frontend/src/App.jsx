8df3d50a821ac9190a7c5b6af1ef1c7b831e460c
Tonnel <154825284+T0nnel@users.noreply.github.com>
2024-07-14 02:55:34 +0300

    Other commits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Landingpage } from './pages/landingpage/Landingpage'
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import {HashRouter as Router,Routes,Route} from "react-router-dom"
 import { Orderpage } from './pages/orderpage/orderpage'
 import { Addproduct } from './pages/addproduct/addproduct'
 import {HomePage} from './pages/homepage/homepage'
@@ -10,14 +10,14 @@ function App() {
   return (
     <>
       <div>
-        <BrowserRouter>
+        <Router>
         <Routes>
           <Route index element={<Landingpage/>}/>
           <Route path="/order" element={<Orderpage/>}/>
           <Route path="/homepage" element={<HomePage/>}/>
           <Route path="/addproduct" element={<Addproduct/>}/>
         </Routes>
-        </BrowserRouter>
+        </Router>
         </div>
     </>
   )
